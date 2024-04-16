229f9602fae85851594e856cccf8df4c5ab2b69e
mshimabukuro <mathiassht@gmail.com>
2024-04-16 13:56:48 -0300

    error de useEffect

--- src/componentes/Principal.jsx
+++ src/componentes/Principal.jsx
@@ -14,7 +14,7 @@ function Main() {
             <Route path='/Contacto' element={ <p>Contacto</p> }/>           
         </Routes>           
            
-                      
+         <ProgramCardContainer/>             
 
         </main>
     )
